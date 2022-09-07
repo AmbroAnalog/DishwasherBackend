0146c471a93aa2cd07b1e7a105803f52f4d6ddf2
Constantin Zborowska <constantin.zborowska@alumni.fh-aachen.de>
2022-09-07 22:36:40 +0200

    add 'get_program_summary' route

--- app/requests.py
+++ app/requests.py
@@ -1,4 +1,7 @@
+from audioop import cross
+from crypt import methods
 import json
+import statistics
 
 import pymongo
 from bson.objectid import ObjectId
@@ -50,6 +53,69 @@ def request_run_list():
         run_list.append(run)
 
     return json.dumps(run_list)
+
+
+@request_bp.route('/get_program_summary/', methods=['GET'])
+@cross_origin()
+def request_program_summary():
+    db = current_app.config['mongo_col']
+
+    runs = db.find({'program_time_end': {'$exists': True, '$type': 'int'}}).sort('session_id', pymongo.DESCENDING)
+
+    program_summary = {}
+    first_pgr_run = 222222222222
+    for run_obj in runs:
+        id = run_obj['program_selected_id']
+        if id in program_summary:
+            # program already exists
+            program_summary[id]['program_counter'] += 1
+            program_summary[id]['program_runtime_list'].append(run_obj['program_runtime'])
+            program_summary[id]['program_estimated_summ'] += run_obj['program_estimated_runtime']
+            if run_obj['machine_aenergy'] is not None:
+                program_summary[id]['program_aenergy_summ'] += run_obj['machine_aenergy']
+                program_summary[id]['program_aenergy_ct'] += 1
+        else:
+            program_summary[id] = {
+                'program_id': id,
+                'program_counter': 1,
+                'program_runtime_list': [run_obj['program_runtime']],
+                'program_estimated_summ': run_obj['program_estimated_runtime']}
+            if run_obj['machine_aenergy'] is not None:
+                program_summary[id]['program_aenergy_summ'] = run_obj['machine_aenergy']
+                program_summary[id]['program_aenergy_ct'] = 1
+            else:
+                program_summary[id]['program_aenergy_summ'] = 0
+                program_summary[id]['program_aenergy_ct'] = 0
+        if run_obj['program_time_start'] < first_pgr_run:
+            first_pgr_run = run_obj['program_time_start']
+
+    program_summary = dict(sorted(program_summary.items()))
+    ret = {}
+    pgr_count = 0
+    run_count = 0
+    aenergy_summ = 0
+    for k, v in program_summary.items():
+        ret[k] = {
+            'program_id': v['program_id'],
+            'program_counter': v['program_counter'],
+            'program_runtime_average': statistics.fmean(v['program_runtime_list']),
+            'program_runtime_stdev': statistics.stdev(v['program_runtime_list']) if len(v['program_runtime_list']) > 1 else 0.0,
+            'program_estimated_average': float(v['program_estimated_summ'] / v['program_counter']),
+            'program_aenergy_average': 0.0,
+            'program_aenergy_summ': v['program_aenergy_summ']
+        }
+        if v['program_aenergy_ct'] > 0:
+            ret[k]['program_aenergy_average'] = float(v['program_aenergy_summ'] / v['program_counter'])
+        pgr_count += 1
+        aenergy_summ += v['program_aenergy_summ']
+        run_count += v['program_counter']
+    
+    return json.dumps({
+        'count_programs': pgr_count, 
+        'count_runs': run_count,
+        'aenergy_summ': aenergy_summ, 
+        'firstrun_time': first_pgr_run,
+        'program_summary': ret})
 
 
 @request_bp.route('/get_latest_run_by_device/<device_oid>', methods=['GET'])
